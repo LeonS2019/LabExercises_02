fb9997e1563fc7926e3fa56bab963c96390a2d33
王振伟 <50258817+Yekongs@users.noreply.github.com>
2019-11-19 13:14:15 +0800

    Create HugeInt.cpp

---
+++ 201816040208/Lab2/HugeInt.cpp
@@ -0,0 +1,233 @@
+// Lab 2: Hugeint.cpp
+// HugeInt member-function and friend-function definitions.
+#include <iostream>
+#include <cctype> // isdigit function prototype
+#include <cstring> // strlen function prototype
+using namespace std;
+
+#include "Hugeint.h" // HugeInt class definition
+
+// default constructor; conversion constructor that converts
+// a long integer into a HugeInt object
+HugeInt::HugeInt( long value )
+{
+   // initialize array to zero
+   for ( int i = 0; i <= 29; i++ )
+      integer[ i ] = 0;
+
+   // place digits of argument into array
+   for ( int j = 29; value != 0 && j >= 0; j-- )
+   {
+      integer[ j ] = value % 10;
+      value /= 10;
+   } // end for
+} // end HugeInt default/conversion constructor
+
+// conversion constructor that converts a character string
+// representing a large integer into a HugeInt object
+HugeInt::HugeInt( const char *string )
+{
+   // initialize array to zero
+   for ( int i = 0; i <= 29; i++ )
+      integer[ i ] = 0;
+
+   // place digits of argument into array
+   int length = strlen( string );
+
+   for ( int j = 30 - length, k = 0; j <= 29; j++, k++ )
+
+      if ( isdigit( string[ k ] ) )
+         integer[ j ] = string[ k ] - '0';
+
+} // end HugeInt conversion constructor
+
+// get function calculates length of integer
+int HugeInt::getLength() const
+{
+    int i;
+   for (  i = 0; i <= 29; i++ )
+      if ( integer[ i ] != 0 )
+         break; // break when first digit is reached
+
+   return 30 - i; // length is from first digit (at i) to end of array
+} // end function getLength
+
+// addition operator; HugeInt + HugeInt
+HugeInt HugeInt::operator+( const HugeInt &op2 ) const
+{
+   HugeInt temp; // temporary result
+   int carry = 0;
+
+   for ( int i = 29; i >= 0; i-- )
+   {
+      temp.integer[ i ] =
+         integer[ i ] + op2.integer[ i ] + carry;
+
+      // determine whether to carry a 1
+      if ( temp.integer[ i ] > 9 )
+      {
+         temp.integer[ i ] %= 10;  // reduce to 0-9
+         carry = 1;
+      } // end if
+      else // no carry
+         carry = 0;
+   } // end for
+
+   return temp; // return copy of temporary object
+} // end function operator+
+
+HugeInt HugeInt::operator-( const HugeInt &op2 ) const
+{
+
+}
+
+// addition operator; HugeInt + int
+HugeInt HugeInt::operator+( int op2 ) const
+{
+   // convert op2 to a HugeInt, then invoke
+   // operator+ for two HugeInt objects
+   return *this + HugeInt( op2 );
+} // end function operator+
+
+HugeInt HugeInt::operator-( int op2 ) const
+{
+
+}
+// addition operator;
+// HugeInt + string that represents large integer value
+HugeInt HugeInt::operator+( const char *op2 ) const
+{
+   // convert op2 to a HugeInt, then invoke
+   // operator+ for two HugeInt objects
+   return *this + HugeInt( op2 );
+} // end function operator+
+
+HugeInt HugeInt::operator-( const char *op2 ) const
+{
+
+}
+
+// equality operator; HugeInt == HugeInt
+/* Write a definition for the == operator */
+bool HugeInt::operator==(const HugeInt &op2) const
+{
+    int i;
+
+    for(i = 29; ((this->integer[i] != 0 || op2.integer[i] != 0) && i >= 0); i--)
+    {
+        if(this->integer[i] != op2.integer[i])
+            return false;
+    }
+
+    return true;
+}
+
+bool HugeInt::operator!=(const HugeInt &op2) const
+{
+    if(*this == op2)
+        return false;
+    else
+        return true;
+}
+// inequality operator; HugeInt != HugeInt
+/* Write a definition for the != operator
+   by calling the == operator */
+
+bool HugeInt::operator<(const HugeInt &op2) const
+{
+    int i = 0, j = 0;
+
+    for(i = 0; i < 30; i++)
+        if(this->integer[i] != 0)
+            break;
+
+    for(j = 0; j < 30; j++)
+        if(op2.integer[j] != 0)
+            break;
+
+    if(i > j)
+        return true;
+    else if(i < j)
+        return false;
+    else
+    {
+        for(; i < 30; i++)
+        {
+            if(this->integer[i] < op2.integer[i])
+                return true;
+            else if(this->integer[i] > op2.integer[i])
+                return false;
+        }
+        return false;
+    }
+}
+// less than operator; HugeInt < HugeInt
+/* Write a definition for the < operator */
+bool HugeInt::operator<=(const HugeInt& op2)    const
+{
+    if(*this < op2 || op2 == *this)
+        return true;
+    else
+        return false;
+}
+
+// less than or equal operator; HugeInt <= HugeInt
+/* Write a definition for the <= operator
+   by calling the < and == operators */
+
+bool HugeInt::operator>(const HugeInt& op2) const
+{
+    if(!( *this < op2 ))
+        return true;
+    else
+        return false;
+}
+
+// greater than operator; HugeInt > HugeInt
+/* Write a definition for the > operator
+   by calling the <= operator */
+
+   bool HugeInt::operator>=(const HugeInt& op2) const
+   {
+       if(*this > op2 || *this == op2)
+        return true;
+       else
+        return false;
+   }
+// greater than or equal operator; HugeInt >= HugeInt
+/* Write a definition for the >= operator
+   by calling the > and == operators */
+
+// overloaded output operator
+ostream& operator<<( ostream &output, const HugeInt &num )
+{
+   int i;
+
+   for ( i = 0; ( num.integer[ i ] == 0 ) && ( i <= 29 ); i++ )
+      ; // skip leading zeros
+
+   if ( i == 30 )
+      output << 0;
+   else
+
+      for ( ; i <= 29; i++ )
+         output << num.integer[ i ];
+
+   return output;
+} // end function operator<<
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
