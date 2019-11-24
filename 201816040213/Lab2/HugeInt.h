5253a1345d8e7c9c291781f8d6ce45d64c5f7e5b
201816040213 <56597300+201816040213@users.noreply.github.com>
2019-11-24 15:41:16 +0800

    Create HugeInt.h
    This is my LabExercises_02
    Student ID : 201816040213

---
+++ 201816040213/Lab2/HugeInt.h
@@ -0,0 +1,38 @@
+// Lab 2: Hugeint.h
+// HugeInt class definition.
+#ifndef HUGEINT_H
+#define HUGEINT_H
+
+#include <iostream>
+using namespace std;
+
+class HugeInt
+{
+   friend ostream &operator<<( ostream &, const HugeInt & );
+public:
+   HugeInt( long = 0 ); // conversion/default constructor
+   HugeInt( const char * ); // conversion constructor
+
+   // addition operator; HugeInt + HugeInt
+   HugeInt operator+( const HugeInt & ) const;
+
+   // addition operator; HugeInt + int
+   HugeInt operator+( int ) const;
+
+   // addition operator;
+   // HugeInt + string that represents large integer value
+   HugeInt operator+( const char * ) const;
+
+   bool operator==( const HugeInt & ) const; // equality operator
+   bool operator!=( const HugeInt & ) const; // inequality operator
+   bool operator< ( const HugeInt & ) const; // less than operator
+   bool operator<=( const HugeInt & ) const; // less than or equal operator
+   bool operator> ( const HugeInt & ) const; // greater than operator
+   bool operator>=( const HugeInt & ) const; // greater than or equal operator
+
+   int getLength() const;
+private:
+   short integer[ 30 ];
+}; // end class HugeInt
+
+#endif
