df99dce35c7b9b0dec9b1343538f20d056db6ba3
201816040229 <56762032+201816040229@users.noreply.github.com>
2019-11-24 22:55:25 +0800

    Create RationalNumber.cpp

---
+++ 201816040229/Lab3/RationalNumber.cpp
@@ -0,0 +1,71 @@
+// Lab 3: RationalNumber.cpp
+// RationalNumber member-function definitions.
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#include "RationalNumber.h"
+
+// RationalNumber constructor sets n and d and calls reduction
+/* Implement the RationalNumber constructor. Validate d first to ensure that
+   it is a positive number and set it to 1 if not. Call the reduction utility
+   function at the end */
+RationalNumber::RationalNumber( int n,int d)
+{
+    numerator=n;
+   if(d>0)
+   {
+       denominator=d;
+
+   }
+   else
+   {
+       denominator=1;
+   }
+   reduction();
+}
+
+// overloaded + operator
+/* Write definition for overloaded operator + */
+RationalNumber RationalNumber::operator+(const RationalNumber &right)
+{
+
+    return RationalNumber(numerator*right.denominator+denominator*right.numerator,denominator*right.denominator);
+}
+// overloaded - operator
+/* Write definition for overloaded operator - */
+RationalNumber RationalNumber::operator-(const RationalNumber &right)
+{
+
+
+    return RationalNumber(numerator*right.denominator-denominator*right.numerator,denominator*right.denominator);
+}
+// overloaded * operator
+/* Write definition for overloaded operator * */
+RationalNumber RationalNumber::operator*(const RationalNumber &right)
+{
+
+    return RationalNumber(numerator*right.numerator,denominator*right.denominator);
+}
+// overloaded / operator
+/* Write definition for overloaded operator /. Check if the client is
+   attempting to divide by zero and report an error message if so */
+RationalNumber RationalNumber::operator/(const RationalNumber &right)
+{
+
+
+    return RationalNumber(numerator*right.denominator, denominator*right.numerator);
+}
+// overloaded > operator
+/* Write definition for operator > */
+bool RationalNumber::operator>(const RationalNumber &right)
+{
+  if(numerator*right.denominator>right.numerator*denominator)
+  {
+      return 1;
+  }
+
+  else
+  {
+      return 0;
+  }
