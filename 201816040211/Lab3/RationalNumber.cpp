29cbff615ed8575a569e79a10cf612b0bb7e9c05
201816040211 <56756575+201816040211@users.noreply.github.com>
2019-11-25 13:25:09 +0800

    Update RationalNumber.cpp

--- 201816040211/Lab3/RationalNumber.cpp
+++ 201816040211/Lab3/RationalNumber.cpp
@@ -1,79 +1,200 @@
-// Lab 3: RationalTest.cpp
-// RationalNumber test program.
-#include <iostream> 
+// Lab 3: RationalNumber.cpp
+// RationalNumber member-function definitions.
+#include <cstdlib>
+#include <iostream>
+#include <string>
 using namespace std;
 
 #include "RationalNumber.h"
 
-int main()
+// RationalNumber constructor sets n and d and calls reduction
+/* Implement the RationalNumber constructor. Validate d first to ensure that
+   it is a positive number and set it to 1 if not. Call the reduction utility
+   function at the end */
+RationalNumber::RationalNumber(int a,int b)
 {
-   RationalNumber c( 7, 3 ), d( 3, 9 ), x;
+    if(b<=0)
+    {
+        b=1;
+    }
+    numerator=a;
+    denominator=b;
+    reduction();
+}//构造函数初始化
 
-   c.printRational();
-   cout << " + ";
-   d.printRational();
-   cout << " = ";
-   x = c + d; // test overloaded operators + and =
-   x.printRational();
+// overloaded + operator
+/* Write definition for overloaded operator + */
+RationalNumber RationalNumber::operator + (RationalNumber &right)
+{
+    RationalNumber h;
+    h.denominator=denominator*right.denominator;
+    h.numerator=numerator*right.denominator+denominator*right.numerator;
+    h.reduction();
+    return h;
+}// 加法的实现
 
-   cout << '\n';
-   c.printRational();
-   cout << " - ";
-   d.printRational();
-   cout << " = ";
-   x = c - d; // test overloaded operators - and =
-   x.printRational();
+// overloaded - operator
+/* Write definition for overloaded operator - */
+RationalNumber RationalNumber::operator - (RationalNumber &right)
+{
+    RationalNumber h;
+    h.denominator=denominator*right.denominator;
+    h.numerator=numerator*right.denominator-denominator*right.numerator;
+    h.reduction();
+    return h;
+}//减法的实现
 
-   cout << '\n';
-   c.printRational();
-   cout << " * ";
-   d.printRational();
-   cout << " = ";
-   x = c * d; // test overloaded operators * and =
-   x.printRational();
+// overloaded * operator
+/* Write definition for overloaded operator * */
+RationalNumber RationalNumber::operator * (RationalNumber &right)
+{
+    RationalNumber h;
+    h.denominator=denominator*right.denominator;
+    h.numerator=numerator*right.numerator;
+    h.reduction();
+    return h;
+}//乘法的实现
 
-   cout << '\n';
-   c.printRational();
-   cout << " / ";
-   d.printRational();
-   cout << " = ";
-   x = c / d; // test overloaded operators / and =
-   x.printRational();
+// overloaded / operator
+/* Write definition for overloaded operator /. Check if the client is
+   attempting to divide by zero and report an error message if so */
+RationalNumber RationalNumber::operator / (RationalNumber &right)
+{
+    RationalNumber h;
+    h.denominator=denominator*right.numerator;
+    h.numerator=numerator*right.denominator;
+    h.reduction();
+    return h;
+}//除法的实现
+// overloaded > operator
+/* Write definition for operator > */
+bool RationalNumber::operator > (RationalNumber &right)
+{
+    double a, b;
+    a=numerator/denominator;
+    b=right.numerator/right.denominator;
+    if(a>b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}//  end function >
 
-   cout << '\n';
-   c.printRational();
-   cout << " is:\n";
+// overloaded < operator
+/* Write definition for operator < */
+bool RationalNumber::operator < (RationalNumber &right)
+{
+    double a, b;
+    a=numerator/denominator;
+    b=right.numerator/right.denominator;
+    if(a<b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}// end function <
 
-   // test overloaded greater than operator
-   cout << ( ( c > d ) ? "  > " : "  <= " );
-   d.printRational();
-   cout << " according to the overloaded > operator\n";
 
-   // test overloaded less than operator
-   cout << ( ( c < d ) ? "  < " : "  >= " );
-   d.printRational();
-   cout << " according to the overloaded < operator\n";
+// overloaded >= operator
+/* Write definition for operator >= */
+bool RationalNumber::operator >= (RationalNumber &right)
+{
+    double a, b;
+    a=numerator/denominator;
+    b=right.numerator/right.denominator;
+    if(a>=b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}// end function >=
 
-   // test overloaded greater than or equal to operator
-   cout << ( ( c >= d ) ? "  >= " : "  < " );
-   d.printRational();
-   cout << " according to the overloaded >= operator\n";
+// overloaded <= operator
+/* Write definition for operator <= */
+bool RationalNumber::operator <= (RationalNumber &right)
+{
+    double a, b;
+    a=numerator/denominator;
+    b=right.numerator/right.denominator;
+    if(a>=b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}// end function <=
 
-   // test overloaded less than or equal to operator
-   cout << ( ( c <= d ) ? "  <= " : "  > " );
-   d.printRational();
-   cout << " according to the overloaded <= operator\n";
-   
-   // test overloaded equality operator
-   cout << ( ( c == d ) ? "  == " : "  != " );
-   d.printRational();
-   cout << " according to the overloaded == operator\n";
+// overloaded == operator
+/* Write definition for operator == */
+bool RationalNumber::operator == (RationalNumber &right)
+{
+    double a, b;
+    a=numerator/denominator;
+    b=right.numerator/right.denominator;
+    if(a==b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}// end function ==
 
-   // test overloaded inequality operator
-   cout << ( ( c != d ) ? "  != " : "  == " );
-   d.printRational();
-   cout << " according to the overloaded != operator" << endl;
-} // end main
+
+// overloaded != operator
+/* Write definition for operator != */
+bool RationalNumber::operator != (RationalNumber &right)
+{
+    double a, b;
+    a=numerator/denominator;
+    b=right.numerator/right.denominator;
+    if(a!=b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}// end function !=
+
+// function printRational definition
+void RationalNumber::printRational() const
+{
+   if ( numerator == 0 ) // print fraction as zero
+      cout << numerator;
+   else if ( denominator == 1 ) // print fraction as integer
+      cout << numerator;
+   else
+      cout << numerator << '/' << denominator;
+} // end function printRational
+
+// function reduction definition
+void RationalNumber::reduction()
+{
+   int largest, gcd = 1;  // greatest common divisor;
+
+   largest = ( numerator > denominator ) ? numerator: denominator;
+
+   for ( int loop = 2; loop <= largest; loop++ )
+       if ( numerator % loop == 0 && denominator % loop == 0 )
+          gcd = loop;
+
+   numerator /= gcd;
+   denominator /= gcd;
+} // end function reduction
 
 
 /**************************************************************************
