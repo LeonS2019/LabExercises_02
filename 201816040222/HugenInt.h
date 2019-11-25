3a869624e335be44eed6a256b1ca51dcec7dddf1
201816040222 <56759713+201816040222@users.noreply.github.com>
2019-11-25 14:12:05 +0800

    Create HugenInt.h

---
+++ 201816040222/HugenInt.h
@@ -0,0 +1,35 @@
+#ifndef HUGEINT_H
+#define HUGEINT_H
+
+#include <iostream>
+using namespace std;
+
+class HugeInt
+{
+   friend ostream &operator<<( ostream &, const HugeInt & );
+public:
+   HugeInt( long = 0 ); // conversion/default constructor
+   HugeInt( const char * ); // conversion constructor
+
+   // addition operator; HugeInt + HugeInt
+   HugeInt operator+( const HugeInt & ) const;
+
+   // addition operator; HugeInt + int
+   HugeInt operator+( int ) const;
+
+   // addition operator;
+   // HugeInt + string that represents large integer value
+   HugeInt operator+( const char * ) const;
+
+   /* Write prototypes for the six relational and equality operators */
+    bool operator==(HugeInt &);
+    bool operator!=(HugeInt &);
+    bool operator<(HugeInt &);
+    bool operator<=(HugeInt &);
+    bool operator>(HugeInt &);
+    bool operator>=(HugeInt &);
+    int getLength() const;
+    int i;
+private:
+   short integer[ 30 ];
+}; // end class HugeInt
